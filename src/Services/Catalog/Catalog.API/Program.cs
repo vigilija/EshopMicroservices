88bf01022bd993a3eb30ac6ff472618781c9c963
Vigilija Vigilija <v.simk@outlook.com>
2025-09-10 18:24:28 +0200

    Add CreateProductCommand and handler for product creation in Catalog.API

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 var app = builder.Build();
 
 // configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
