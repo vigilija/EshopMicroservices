ea2d351250949e7ed2f22d81a1e10bd4c5a72d52
Vigilija Vigilija <v.simk@outlook.com>
2025-09-11 08:42:36 +0200

    added db to Handel method

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using MediatR;
+﻿
 
 namespace Catalog.API.Products.CreateProduct
 
@@ -9,7 +7,7 @@ namespace Catalog.API.Products.CreateProduct
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler 
+    internal class CreateProductCommandHandler(IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -24,7 +22,10 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(prodduct);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(prodduct.Id);
         }
     }
 }
