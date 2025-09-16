235be4127d9656f8a4446afff80016d9bb464165
Vigilija Vigilija <v.simk@outlook.com>
2025-09-16 23:08:36 +0200

    Implementing create product handler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
