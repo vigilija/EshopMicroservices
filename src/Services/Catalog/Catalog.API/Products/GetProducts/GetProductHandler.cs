25bb031d09cbc81b04d2c1437045e96b52ab1f96
Vigilija Vigilija <v.simk@outlook.com>
2025-09-16 23:07:47 +0200

    Implementing Get Prody=ucts classes

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductHandler.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+
+public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) 
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handling GetProductsQuery {@Query}", query);
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
+    }
+}
