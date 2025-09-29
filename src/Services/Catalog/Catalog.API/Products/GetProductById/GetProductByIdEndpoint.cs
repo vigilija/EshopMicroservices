3febfb8d152146f14fb22f0f4175ea75f6cc504a
Vigilija Vigilija <v.simk@outlook.com>
2025-09-29 21:16:06 +0200

    added GetProductById endpoint

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,6 +1,30 @@
-﻿namespace Catalog.API.Products.GetProductById
+﻿
+namespace Catalog.API.Products.GetProductById;
+//public record GetProductByIdRequest(Guid Id);
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+
 {
-    public class GetProductByIdEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var query = new GetProductByIdQuery(id);
+            var result = await sender.Send(query);
+
+            if (result == null)
+            {
+                return Results.NotFound(new { Message = "Product not found" });
+            }
+
+            var response = result.Adapt<GetProductByIdResponse>();           ;
+            return Results.Ok(response);
+        })
+            .WithName("GetProductById")
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get product by ID")
+            .WithDescription("Retrieves a product by its unique ID.");
     }
 }
