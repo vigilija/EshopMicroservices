45de790c61358a1fe3152daa9abd60b2734a3025
Vigilija Vigilija <v.simk@outlook.com>
2025-09-30 23:44:46 +0200

    implemented Delete Product Handler class

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DelegateProductResult>;
+
+public record DelegateProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler
+    (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DelegateProductResult>
+{
+    public async Task<DelegateProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductCommand.Handle called with {@Command}", command);
+
+        session.Delete<Product>(command);
+
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DelegateProductResult(true);
+
+    }
+}
