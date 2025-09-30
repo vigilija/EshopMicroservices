780e26f970607fd33f8824a1e16a761806904b02
Vigilija Vigilija <v.simk@outlook.com>
2025-09-30 23:53:08 +0200

    implemented Delete Product Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+//public record DeleteProductRequest(Guid id);
+
+public record DeleteProductResponc(bool isSuccess);
+
+public class DeleteProductEndpoint
+    : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var command = new DeleteProductCommand(id);
+            var result = await sender.Send(command);
+            var response = result.Adapt<DeleteProductResponc>();
+            if (response is null)
+            {
+                return Results.BadRequest(new { Message = "Failed to delete product" });
+            }
+            return Results.Ok(response);
+        })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponc>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete a product");
+    }
+}
