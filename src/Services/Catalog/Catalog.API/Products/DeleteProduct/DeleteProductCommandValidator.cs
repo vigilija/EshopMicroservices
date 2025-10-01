da41b71ffdcad4f949d2ba4b9a9fa006387697e7
Vigilija Vigilija <v.simk@outlook.com>
2025-10-01 16:24:19 +0200

    Added Delete Product and Update Product validators

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommandValidator: AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required.");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Product name is required.");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("At least one category is required.");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Product description is required.");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image file URL is required.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than zero.");
+    }
+}
